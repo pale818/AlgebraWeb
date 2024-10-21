d00045ff01dc8168fbc937ba7ccb788ef611345b
pcaric <pcaric@algebra.hr>
2024-10-21 21:43:44 +0200

    bootstram import

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-/*
-var x corresponds to the topnav element in teh HTML file
-if it was topnam on click it becomes responsive and gain on click it becomes topnav
-and so on, this function changes class from topnav to responsive and vice versa
-*/
-function toggleMenu() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+
+// Function to dynamically load content into the modal based on what the user clicked
+function loadModalContent(type) {
+  const modalTitle = document.getElementById('unifiedModalLabel');
+  const modalBody = document.getElementById('modalBodyContent');
+
+  if (type === 'contact') {
+      modalTitle.textContent = 'Contact Us';
+      modalBody.innerHTML = '<iframe src="PAGES/contact.html" width="100%" height="500px" style="border:none;"></iframe>';
+  } else if (type === 'login') {
+      modalTitle.textContent = 'Login/Register';
+      modalBody.innerHTML = '<iframe src="PAGES/login.html" width="100%" height="500px" style="border:none;"></iframe>';
   } else {
-    x.className = "topnav";
-  }
+    modalTitle.textContent = 'Under Construction';
+    modalBody.innerHTML = '<iframe src="PAGES/underConstruction.html" width="100%" height="500px" style="border:none;"></iframe>';
+}
+
+  // Open the modal
+  const modal = new bootstrap.Modal(document.getElementById('unifiedModal'));
+  modal.show();
 }
